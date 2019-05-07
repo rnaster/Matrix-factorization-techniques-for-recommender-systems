0ba7b24429f9096ef95cdb60568d999e9a31f585
yun <blpomspo@gmail.com>
2019-05-08 04:30:58 +0900

    implement matrix factorization with keras
    - visualization
      - cost according to epoch
      - when latent dimension: 2, draw scatter plot for user vector and item vector
    - reproducible
    - predict for test set and non-observed data
      - recommend top 3 item for each user

--- src/model.py
+++ src/model.py
@@ -1,4 +1,11 @@
+# TODO: split train, test
+import pathlib
 import numpy as np
+import pandas as pd
+import tensorflow as tf
+import matplotlib.pyplot as plt
+
+from src.pipeline import DataPipeline
 from src.visual import Visualization
 
 
@@ -82,6 +89,159 @@ class VanillaMF(Visualization):
         predicted_matrix = self.predict_matrix * mask_matrix * -1
         index = predicted_matrix[user].argsort()
         return index[:num_rec_items]
+
+
+class KerasVanillaMF:
+    def __init__(self,
+                 data: pd.DataFrame,
+                 seed=None):
+        """
+
+        :param data: narrow data, column = [user, item, rating]
+        """
+        self.data = data
+        self.seed = seed
+        self.model = None
+        self.recommendation = pd.DataFrame(columns=['top1', 'top2', 'top3'])
+
+    def run(self,
+            latent_dim,
+            l2_rate,
+            epoch,
+            batch_size,
+            verbose=2,
+            save=False):
+        dataset = self.data_flow(batch_size)
+        self.build(latent_dim, l2_rate)
+        self.train(dataset.train_steps,
+                   dataset.val_steps,
+                   epoch,
+                   verbose)
+        self.evaluate_test(dataset.test_steps, dataset.pre_test_set)
+        self.plot_cost(save)
+        if latent_dim == 2: self.plot_embedded_vector(save)
+        self.predict()
+
+    def evaluate_test(self, test_steps, pre_test_set):
+        _, _, test_set = tf.get_collection('dataset')[0]
+        self.model.evaluate(test_set, steps=test_steps)
+        self.data['predicted'] = 0
+        predicted_test = self.model.predict(test_set, steps=test_steps).reshape(-1)
+        self.data = self.data.set_index(['user', 'item'])
+        for i in range(len(pre_test_set)):
+            self.data.loc[(pre_test_set[i][0], pre_test_set[i][1]), 'predicted'] = predicted_test[i]
+
+    def predict(self):
+        self.data = self.data.reset_index()
+        item_set = set(self.data['item'])
+        for user in self.data['user'].unique():
+            item2recommend = item_set - set(self.data[self.data['user'] == user]['item'])
+            item2recommend = list(item2recommend)
+            dataset = self.make_dataset(user, item2recommend)
+            result = self.model.predict(dataset)
+            idx = (-result).reshape(-1).argsort()[:3]
+            self.recommendation.loc[user] = [item2recommend[i] for i in idx]
+
+    def make_dataset(self, user, item_list):
+        user = [user] * len(item_list)
+        return user, item_list
+
+    def plot_embedded_vector(self, save=False):
+        user_vector, item_vector = self.model.get_weights()
+        plt.plot(*user_vector.T,
+                 marker='o',
+                 ls='',
+                 c='b',
+                 markersize=1,
+                 label='user')
+        plt.plot(*item_vector.T,
+                 marker='o',
+                 ls='',
+                 c='r',
+                 markersize=1,
+                 label='item')
+        plt.legend(loc='upper right',
+                   bbox_to_anchor=(1, 1.1),
+                   ncol=2,
+                   handletextpad=0.01)
+        plt.title('Latent Vector Space')
+        plt.axhline(0, color='g', ls='-')
+        plt.axvline(0, color='g', ls='-')
+        if save: plt.savefig('./book_crossing/latent_space.jpg')
+        plt.show()
+        plt.close()
+
+    def plot_cost(self, save=False):
+        pd.DataFrame(self.model.history.history).plot()
+        plt.xlabel("Epoch")
+        plt.ylabel("loss")
+        if save: plt.savefig('./book_crossing/plot_cost.jpg')
+        plt.show()
+        plt.close()
+
+    def data_flow(self, batch_size):
+        dataset = DataPipeline(self.data.values,
+                               batch_size=batch_size,
+                               seed=self.seed)
+        train_set, val_set, test_set = dataset.dataset()
+        tf.add_to_collection('dataset',
+                             [train_set, val_set, test_set])
+        return dataset
+
+    def build(self, latent_dim, l2_rate=0):
+        users = max(self.data.user.unique())
+        items = max(self.data.item.unique())
+
+        user_input = tf.keras.layers.Input(shape=(1,), name="user")
+        user_vec = self.embedding(user_input, users, latent_dim, l2_rate)
+
+        item_input = tf.keras.layers.Input(shape=(1,), name="item")
+        item_vec = self.embedding(item_input, items, latent_dim, l2_rate)
+
+        dot_product = tf.keras.layers.Dot(axes=1)([user_vec, item_vec])
+        model = tf.keras.models.Model([user_input, item_input], dot_product)
+        model.compile('adam', 'mse')
+
+        model.summary()
+        self.model = model
+
+    def train(self, train_steps, val_steps, epoch, verbose=2):
+        train_set, val_set, _ = tf.get_collection('dataset')[0]
+        self.model.fit(train_set.make_one_shot_iterator(),
+                       epochs=epoch,
+                       validation_data=val_set.make_one_shot_iterator(),
+                       validation_steps=val_steps,
+                       steps_per_epoch=train_steps,
+                       verbose=verbose,
+                       shuffle=False)
+        self.save_model('./book_crossing/weights')
+
+    def save_model(self, path):
+        pathlib.Path(path) \
+            .mkdir(parents=True,
+                   exist_ok=True)
+        self.model.save(path + '/model')
+
+    def embedding(self,
+                  last_layer,
+                  input_dim,
+                  latent_dim,
+                  l2_rate):
+        input_length = 1
+        if l2_rate > 0:
+            regularizer = tf.keras.regularizers.l2(l2_rate)
+        else:
+            regularizer = None
+        initializer = tf.keras\
+            .initializers\
+            .RandomUniform(seed=self.seed)
+        embedding = tf.keras.layers.Embedding(
+                input_dim+1,
+                latent_dim,
+                input_length=input_length,
+                embeddings_initializer=initializer,
+                embeddings_regularizer=regularizer)(last_layer)
+        return tf.keras.layers.Flatten()(embedding)
 
 
 if __name__ == '__main__':
